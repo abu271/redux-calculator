86d8d0a6834817e22f132b8bb6132fec5872fe4a
abuDarda97 <abudarda166@gmail.com>
2019-04-21 21:04:17 +0100

    final version

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import * as constant from '../actions/constants'
 import CalculatorMethods from '../../calculator/CalculatorMethods'
 
 const initialState = {
-    value: 'initial state',
+    value: '',
     expression: ''
 }
 
@@ -10,29 +10,68 @@ const calculator = new CalculatorMethods()
 
 const rootReducer = (state = initialState, action) => {
     switch(action.type) {
+
         case constant.ADD:
             calculator.add()
-            return {...state, expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue().toString(),
+                    expression: calculator.getExpression()
+                }
+
         case constant.MULTIPLY:
             calculator.multiply()
-            return {...state, expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue().toString(),
+                    expression: calculator.getExpression()
+                }
+
         case constant.DIVIDE: 
             calculator.divide()
-            return {...state, expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue().toString(),
+                    expression: calculator.getExpression()
+                }
+
         case constant.SUBTRACT:
             calculator.subtract()
-            return {...state, expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue().toString(),
+                    expression: calculator.getExpression()
+                }
+
         case constant.INPUT_NUMBER:
-            return {...state, value: calculator.getValue()}
+            return {
+                    ...state,
+                    value: calculator.getValue()
+                }
+
         case constant.INPUT_DECIMAL:
             calculator.inputDecimal()
-            return { ...state, value: calculator.getValue().toString()}
+            return {
+                    ...state,
+                    value: calculator.getValue().toString()
+                }
+
         case constant.CLEAR_ALL:
             calculator.clearAll()
-            return {...state, value: calculator.getValue(), expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue(),
+                    expression: calculator.getExpression().toString()
+                }
+
         case constant.EQUAL:
             calculator.equals()
-            return { ...state, value: calculator.getValue(), expression: calculator.getExpression()}
+            return {
+                    ...state,
+                    value: calculator.getValue(),
+                    expression: calculator.getExpression().toString()
+                }
+
         default:
             return state
     }
